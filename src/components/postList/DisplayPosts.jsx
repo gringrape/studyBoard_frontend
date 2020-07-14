5f717d5bf0c61be8aa7a992cf75f41f5557402ad
gringrape <53764714+gringrape@users.noreply.github.com>
2020-07-14 16:28:21 +0900

    refactor infinite scroll

--- src/components/postList/DisplayPosts.jsx
+++ src/components/postList/DisplayPosts.jsx
@@ -1,77 +1,75 @@
-import React, { useState, useEffect } from 'react';
-import { useImperativeQuery } from '../../utils/queryUtils.js';
-import { useInView } from 'react-intersection-observer';
-import { gql } from 'apollo-boost';
+import React, { useEffect } from 'react';
 import DisplayPost from './DisplayPost.jsx';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
 import './DisplayPosts.scss';
 
-const POST_LIST = gql`
-	query GetPosts($page: Int!, $per: Int!, $tag: String, $titleQuery: String) {
-		getPosts(page: $page, per: $per, tag: $tag, titleQuery: $titleQuery) {
-			id
-			title
-			content
-			tags
-			at
-		}
-	}
-`;
-
 const DisplayPosts = ({ tag, titleQuery, setMounted }) => {
-	const callQuery = useImperativeQuery(POST_LIST);
-	const [ posts, setPosts ] = useState([]);
-	const [ page, setPage ] = useState(0);
-	const [ prevY, setPrevY ] = useState(0);
-	const [ ref, inView, entry ] = useInView({
-		/* Optional options */
-		threshold: 0
-	});
-
-	const fetchPosts = async (page, per) => {
-		const { data, error } = await callQuery({ page, per, tag, titleQuery });
-		if (data) {
-			setPosts(posts.concat(data.getPosts));
-			setPage(page + 1);
+	const POST_LIST = gql`
+		query GetPosts($offset: Int!, $limit: Int!, $tag: String, $titleQuery: String) {
+			getPosts(offset: $offset, limit: $limit, tag: $tag, titleQuery: $titleQuery) {
+				id
+				title
+				content
+				tags
+				at
+			}
 		}
-		// TODO: error 발생시의 대처
+	`;
+
+	window.onscroll = function(ev) {
+		const scrollReachesBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight;
+		if (scrollReachesBottom) {
+			fetchMore({
+				variables: {
+					offset: data.getPosts.length
+				},
+				updateQuery: (prev, { fetchMoreResult }) => {
+					console.log(prev);
+					if (!fetchMoreResult) {
+						return prev;
+					}
+					return {
+						getPosts: [ ...prev.getPosts, ...fetchMoreResult.getPosts ]
+					};
+				}
+			});
+		}
 	};
 
 	useEffect(() => {
-		// component did mount
 		setMounted(true);
-		fetchPosts(page, 10);
 		return () => {
-			// component will unmount
 			setMounted(false);
-		};
+		}
 	}, []);
 
-	useEffect(
-		() => {
-			if (entry && inView) {
-				const bodyRect = document.body.getBoundingClientRect();
-				const elemRect = entry.target.getBoundingClientRect();
-				const y = elemRect.top - bodyRect.top;
-				if (prevY < y) {
-					fetchPosts(page, 10);
-					setPrevY(y);
-				}
-			}
-		},
-		[ inView ]
-	);
+	const { loading, error, data, fetchMore } = useQuery(POST_LIST, {
+		variables: {
+			offset: 0,
+			limit: 10,
+			tag,
+			titleQuery
+		}
+	});
+
+	if (loading) {
+		return <p>loading...</p>;
+	}
+
+	if (error) {
+		return <p>error :(</p>;
+	}
 
 	return (
 		<div className="list-container">
 			<div>
-				{posts.map((post, i) => (
+				{data.getPosts.map((post, i) => (
 					<React.Fragment>
 						<DisplayPost className="post-box" key={i} data={post} />
-						<hr className="post-divider"/>
 					</React.Fragment>
 				))}
 			</div>
-			<br ref={ref} />
 		</div>
 	);
 };
